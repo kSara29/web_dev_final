b638998e9d28b87d31e8cee1ce1bd54808cbdeb0
Kuandykova Sara <kuandykovasara29@gmail.com>
2023-12-18 21:52:33 +0600

    bug fix

--- Instagram/obj/Debug/net7.0/Instagram.AssemblyInfo.cs
+++ Instagram/obj/Debug/net7.0/Instagram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3778ea198b075c5e3bac64865a4302fbf42f2cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568cb7f9e6cf8a4011bb97797ee2c64774f4655d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
